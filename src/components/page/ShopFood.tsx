56f45a549cf7b5e7f97c6e53b52b5e7efb7b56a1
amirabas <abasaarafi@outlook.com>
2024-03-05 14:41:23 +0330

    create ui for shopfood,tsx

--- src/components/page/ShopFood.tsx
+++ src/components/page/ShopFood.tsx
@@ -1,10 +1,16 @@
-
+import imgCarde from '../../'
 
 function ShopFood() {
   return (
-    <div>
-      <button className="btn btn-danger ">fastfood shoping</button>
+    <div className="card" style={{width: '19rem'}}>
+      <img src="" className="card-img-top" alt="not_found">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
     </div>
+
   )
 }
 
