12d2aa6fde6053ba7258a4242c6a565a5608b816
amirabas <abasaarafi@outlook.com>
2024-03-08 20:07:05 +0330

    create item loadte

--- src/components/page/ShopFood.tsx
+++ src/components/page/ShopFood.tsx
@@ -1,29 +1,23 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 
 export default function ShopFood() {
-  const [dataPost,setDataPost] = useState([])
+  // const [dataPost,setDataPost] = useState([])
 
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => setDataPost(data))
-  })
+  // useEffect(()=>{
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then(response => response.json())
+  //     .then(data => setDataPost(data))
+  // })
   
   return (
     <div className="card w-100 d-flex flex-row h-auto ">
-        {
-          dataPost.map((item:any) => {
-            return (
-              <div className="container mx-auto my-5">
-              <div className="card-body">
-                <h5 className="card-title">{item.title}</h5>
-                <p className="card-text">{item.body}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
+      <div className="container mx-auto my-5">
+        <div className="card-body">
+          <h5 className="card-title">data this is a cat the cat is big</h5>
+          <p className="card-text">contaciner lik and man to be left conact</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
-            )
-          })
-        }
+      </div>
       </div>
   )
 }
