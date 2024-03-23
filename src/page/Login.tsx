8cc1b4090543c5893e57502635934151095f993b
amirabas <abasaarafi@outlook.com>
2024-03-23 23:34:08 +0330

    git add page login

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,10 +1,14 @@
 import styles from "../assets/sass/login.module.scss"
 
+
 function Login() {
   return (
     <div className={styles.login_container}>
-      <div className="login_content">
+      <div>
         <h2>سلام میتونم کمکتون کنم</h2>
+      </div>
+      <div>
+
       </div>
     </div>
   )
