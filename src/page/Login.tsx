d059e9c63511988226c3c59dd8ef919de5000222
amirabas <abasaarafi@outlook.com>
2024-03-24 11:50:41 +0330

    not use formike

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,14 +1,22 @@
-import styles from "../assets/sass/login.module.scss"
+import { Formik,Field } from "formik"
 
 
 function Login() {
-  return (
-    <div className={styles.login_container}>
-      <div>
-        <h2>سلام میتونم کمکتون کنم</h2>
-      </div>
-      <div>
 
+  return (
+    <div className="login_container">
+      <div className="login_container">
+        <h3 className="text-withe">وارد شوید</h3>
+        <Formik
+          initialValues={{name: "",email: ""}}
+          onSubmit={(values)=>{
+            console.log("Form inputs data => ", values);
+          }}
+        >
+          <Field type="text" name="name" placeholder="name"  />
+          <Field type="email" name="email" placeholder="email" />
+
+        </Formik>
       </div>
     </div>
   )
